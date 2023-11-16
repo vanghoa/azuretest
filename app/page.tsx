040d1e018d8a6827206404e37480a38178c2c38e
bao ann <baoanh1buinguyen@gmail.com>
2023-11-16 16:40:48 +0800

    yeehaw

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image';
 
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
-export const fetchCache = 'force-no-store';
 
 async function getData() {
     try {
